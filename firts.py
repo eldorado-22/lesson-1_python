62cd036acc7d8b512776787adc2c7694e282398c
eldorado-22 <jumashevv992@gmail.com>
2023-08-31 11:48:44 +0600

    lesson-1

---
+++ firts.py
@@ -0,0 +1,10 @@
+# word = "Python"
+# for letter in word: 
+#     print(letter)
+
+# print("Hello Word")
+
+# print(type(5)) -> <class "int">
+# print(type("Hello"))<class 'str'>
+# print(1 < 5) # true
+# print(1 > 5) # false
